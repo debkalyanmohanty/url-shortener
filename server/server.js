82e937d28e3418003c1841f0500d0d070e691677
Deb Kalyan <Deb.Kalyan@markoknow.com>
2024-05-14 22:32:59 +0530

    Completed The Whole Shortener

--- server/server.js
+++ server/server.js
@@ -4,9 +4,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes/route');
+const app = express();
+const server = require("http").createServer(app);
 
 const MONGO_URI = process.env.MONGO_URI;
-const app = express();
+
 
 //form-data
 app.use(express.urlencoded({ extended: false }));
@@ -20,14 +22,21 @@ mongoose.connect(MONGO_URI , {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+mongoose.connection.on("connected", async () => {
+    console.log("connected to mongo ");
+  });
+  mongoose.connection.on("error", async (err) => {
+    console.log("error connecting", err);
+  });
+
+
+app.use('/api' , routes);
 
 app.get('/' , (req , res) => {
     res.send('Server Live')
 })
 
-app.use('/api' , routes);
-
-app.listen(process.env.PORT || 3002 , () => {
+server.listen(process.env.PORT || 3002 , () => {
     console.log('Server Is Live')
 });
 
